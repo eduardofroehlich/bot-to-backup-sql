c6e09dededd6ccfd6202f5dc8d34fe97eacb26d9
Envios da VPS <eduardosilvafroehlich@gmail.com>
2024-04-10 11:03:13 -0300

    Add more conditions to see error in terminal
    Signed-off-by: Envios da VPS <eduardosilvafroehlich@gmail.com>

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
 const { MessageEmbed, WebhookClient } = require('discord.js');
 const mysqldump = require('mysqldump');
 const config = require('./config');
+var hasError = false;
 
 if (config.webhook === "") {
     console.log('%c' + config.messages.error.webhook, 'color: #FF0000;');
-    return;
+    hasError = true;
 }
 
 if (config.connection.host === "0.0.0.0" || config.connection.user === "" || config.connection.database === "") {
     console.log('%c' + config.messages.error.connection, 'color: #FF0000;');
-    return;
+    hasError = true;
 }
 
-setInterval(async() => {
-    mysqldump({
-        connection: {
-            host: config.connection.host,
-            user: config.connection.user,
-            password: config.connection.password,
-            database: config.connection.database,
-
-        },
-        dumpToFile: './backup.sql'
-    });
-
-    const webhookClient = new WebhookClient({ id: config.webhook.split('https://discord.com/api/webhooks/')[1].split('/')[0], token: config.webhook.split('https://discord.com/api/webhooks/')[1].split('/')[1] });
-    let time = Math.round(+new Date() / 1000)
-    const embed = new MessageEmbed()
-        .setTitle(`${config.messages.title} | ${config.connection.database}`)
-        .setColor(config.embed.color)
-        .setDescription(`${config.messages.description} <t:${time}>`)
-
-    setTimeout(() => {
-        webhookClient.send({
-            username: config.botusername,
-            embeds: [embed],
-            files: ['./backup.sql']
+if (!hasError) {
+    setInterval(async() => {
+        mysqldump({
+            connection: {
+                host: config.connection.host,
+                user: config.connection.user,
+                password: config.connection.password,
+                database: config.connection.database,
+    
+            },
+            dumpToFile: './backup.sql'
         });
-    }, 15000)
-
-
-}, 1000 * 60 * config.time)
-
-console.log(`%c${config.messages.online}`, 'background: green; color: white; display: block;');
-console.log(`%c${config.messages.online}`, 'color: white; background-color: #008000;');
+    
+        const webhookClient = new WebhookClient({ id: config.webhook.split('https://discord.com/api/webhooks/')[1].split('/')[0], token: config.webhook.split('https://discord.com/api/webhooks/')[1].split('/')[1] });
+        let time = Math.round(+new Date() / 1000)
+        const embed = new MessageEmbed()
+            .setTitle(`${config.messages.title} | ${config.connection.database}`)
+            .setColor(config.embed.color)
+            .setDescription(`${config.messages.description} <t:${time}>`)
+    
+        setTimeout(() => {
+            webhookClient.send({
+                username: config.botusername,
+                embeds: [embed],
+                files: ['./backup.sql']
+            });
+        }, 15000)
+    
+    
+    }, 1000 * 60 * config.time)
+    
+    console.log(`%c${config.messages.online}`, 'color: white; background-color: #008000;');
+} 
+else {
+    setInterval(async() => {
+    }, 1000)
+}
