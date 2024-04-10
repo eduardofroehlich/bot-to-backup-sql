51527d700f9efaef0c2f35684a217170a4fcb222
Envios da VPS <eduardosilvafroehlich@gmail.com>
2024-04-10 11:15:34 -0300

    Fix connection error message
    Signed-off-by: Envios da VPS <eduardosilvafroehlich@gmail.com>

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-    webhook: "", // webhook url what you get in discord channel
+    webhook: "https://discord.com/api/webhooks/1227606547421593721/lPeKJO7bU0LW-_741irHkuwwg0-pBT1QhFZPo03YtWSQoE1agjXHRNVKIH31uyG83NYA", // webhook url what you get in discord channel
     time: 240, // in min
     botusername: "Peepo Backuper", //Bot Name
 
@@ -18,7 +18,7 @@ const config = {
         online: "[ONLINE] The bot is running.",
         error: {
             webhook: "Please enter the webhook url in the config.js file.",
-            connection: "Please enter the connections informations in the config.js file."
+            connection: "Please configure the connections informations in your .env"
         },
     },
 
